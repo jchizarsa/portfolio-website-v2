54b48900c94013daeef60663a88e7191803446ac
JC <75799709+jchizarsa@users.noreply.github.com>
2022-08-21 16:14:22 -0700

    Pivot to using TailwindCSS

---
+++ portfolio-react-app/src/constants/images.js
@@ -0,0 +1,7 @@
+import logodark1024 from '../assets/logodark1024x1024.png';
+import logolight1024 from '../assets/logolight1024x1024.png';
+
+export default{
+    logodark1024,
+    logolight1024
+};
